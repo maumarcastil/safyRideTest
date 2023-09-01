678ee188deb02c11df56f356585e03b7b956db0c
Mauricio Martinez <maumarcastil@gmail.com>
2023-09-01 11:35:56 -0500

    feat: configure axios

--- src/main.tsx
+++ src/main.tsx
@@ -8,11 +8,13 @@ import "./index.css";
 import App from "./App.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
+  <>
+    {/* <React.StrictMode> */}
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
         <App />
       </PersistGate>
     </Provider>
-  </React.StrictMode>
+    {/* </React.StrictMode> */}
+  </>
 );
