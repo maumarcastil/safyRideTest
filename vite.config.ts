d1ed47791170af1c9b13f70cb174e1169edd6789
Mauricio Martinez <maumarcastil@gmail.com>
2023-09-01 11:35:10 -0500

    feat: configure relative paths and install react icons

--- vite.config.ts
+++ vite.config.ts
@@ -6,4 +6,9 @@ import svgr from "vite-plugin-svgr";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svgr(), react()],
+  resolve: {
+    alias: {
+      '@': '/src',
+    }
+  }
 })
