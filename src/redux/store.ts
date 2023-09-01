141946e248d93b98dc1ab1b99243e176233eb747
Mauricio Martinez <maumarcastil@gmail.com>
2023-09-01 11:40:04 -0500

    feat: hook redux

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,21 +1,22 @@
-import {configureStore} from '@reduxjs/toolkit';
-import storage from 'redux-persist/lib/storage';
-import { persistStore, persistReducer } from 'redux-persist';
+import { configureStore } from "@reduxjs/toolkit";
+import storage from "redux-persist/lib/storage";
+import { persistStore, persistReducer } from "redux-persist";
 
-import { rootReducer } from './reducers';
+import { rootReducer } from "./reducers";
 
 const persistConfig = {
   key: "root",
   storage,
-  whitelist: ["characters"]
-}
+};
 
-const persistedReducer = persistReducer(persistConfig, rootReducer)
-
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
-    reducer: persistedReducer,
-    middleware: (getDefaultMiddleware) => getDefaultMiddleware(),
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+    }),
 });
 
 export const persistor = persistStore(store);
