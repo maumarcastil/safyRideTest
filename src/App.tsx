87653c9a577e496192cf1ffc72b4a3f4a4a0a192
Mauricio Martinez <maumarcastil@gmail.com>
2023-09-01 23:47:12 -0500

    feat: use slider in app

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,30 @@
-import { useState } from "react";
-import "./App.css";
+import React, { Suspense, useEffect } from "react";
+import { useAppDispatch, useAppSelector } from "./hooks/redux";
+import { fetchCharacters } from "./redux/slices/characters/charactersActions";
+
+const LazySlider = React.lazy(() => import("@/components/slider"));
 
 function App() {
-  const [count, setCount] = useState(0);
+  const { currentCharacter } = useAppSelector((state) => state.characters);
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    (async () => {
+      if (!currentCharacter) {
+        await dispatch(fetchCharacters(1));
+      }
+    })();
+  }, [dispatch]);
 
   return (
     <>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+      <div className="flex w-full h-full justify-center items-center">
+        <div className="max-w-[1440px] mx-2 my-4">
+          <Suspense fallback={<div>Loading...</div>}>
+            <LazySlider />
+          </Suspense>
+        </div>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   );
 }
